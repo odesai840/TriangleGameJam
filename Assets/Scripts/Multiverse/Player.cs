dba50e7861f55636ad1579e8abd1cd76a25de89c
macsimilian <98413300+maxlevinestuff@users.noreply.github.com>
2025-03-01 16:29:59 -0500

    ship steering physics

--- Assets/Scripts/Multiverse/Player.cs
+++ Assets/Scripts/Multiverse/Player.cs
@@ -1,28 +1,59 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class Player : MonoBehaviour
 {
-    public float moveSpeed = 5f;
-    private Rigidbody2D rb;
-    private Vector2 movement;
+    [Header("Movement Settings")]
+    public float rotationSpeed = 180f;   // degrees per second
+    public float acceleration = 5f;      // force for forward movement
+    public float deceleration = 5f;      // force for deceleration
 
-    void Start()
+    private Rigidbody2D rb;
+
+    void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        // Usually for top-down, we don't want gravity
+        rb.gravityScale = 0f;
+        // Optionally set constraints if needed:
+        // rb.constraints = RigidbodyConstraints2D.FreezeRotation; // if you want to handle rotation manually
     }
 
     void Update()
     {
-        // Get input
-        float moveX = Input.GetAxis("Horizontal");
-        float moveY = Input.GetAxis("Vertical");
+        // ROTATION
+        // Left arrow => rotate left, Right arrow => rotate right
+        float rotation = 0f;
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rotation = rotationSpeed;  // rotate left
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rotation = -rotationSpeed; // rotate right
+        }
 
-        movement = new Vector2(moveX, moveY);
+        // Apply rotation in degrees per second
+        // (We'll do it in Update, then apply in FixedUpdate for precise physics)
+        // For a simple approach, we can do rotation in Update if the object is not moving extremely fast
+        float rotThisFrame = rotation * Time.deltaTime;
+        transform.Rotate(0, 0, rotThisFrame);
+
+        // ACCELERATION & DECELERATION
+        // Up => accelerate forward, Down => decelerate
+        // We'll store that as well, then apply in FixedUpdate
     }
 
     void FixedUpdate()
     {
-        // Apply movement
-        rb.velocity = movement * moveSpeed;
+        // For forward direction, we use transform.up in 2D
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            rb.velocity += (Vector2)transform.up * (acceleration * Time.fixedDeltaTime);
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            rb.velocity -= (Vector2)transform.up * (deceleration * Time.fixedDeltaTime);
+        }
     }
 }
