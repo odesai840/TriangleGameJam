60a83389defd97e3e71212fee1de93c510560bfe
odesai840 <odesai840@gmail.com>
2025-03-02 10:54:09 -0500

    adding in sprites

--- Assets/Scripts/Universes/Environment/AsteroidSpawner.cs
+++ Assets/Scripts/Universes/Environment/AsteroidSpawner.cs
@@ -8,8 +8,6 @@ public class AsteroidSpawner : MonoBehaviour
     [SerializeField] private GameObject asteroidPrefab;
     [SerializeField] private float spawnIntervalMin = 1.0f;
     [SerializeField] private float spawnIntervalMax = 3.0f;
-    [SerializeField] private float randomRotationMin = 0.0f;
-    [SerializeField] private float randomRotationMax = 90.0f;
 
     private void Start()
     {
@@ -19,8 +17,7 @@ public class AsteroidSpawner : MonoBehaviour
     private IEnumerator SpawnRoutine()
     {
         yield return new WaitForSeconds(Random.Range(spawnIntervalMin, spawnIntervalMax));
-        float randomRotation = Random.Range(randomRotationMin, randomRotationMax);
-        Instantiate(asteroidPrefab, transform.position, Quaternion.AngleAxis(randomRotation, Vector3.forward));
+        Instantiate(asteroidPrefab, transform.position, Quaternion.Euler(0, 0, 180));
         StartCoroutine("SpawnRoutine");
     }
 }
