6964b17f1425276c7d08af66ea0a010e9063363d
macsimilian <98413300+maxlevinestuff@users.noreply.github.com>
2025-03-01 20:43:20 -0500

    transition effect progress

---
+++ Assets/Scripts/Multiverse/CameraChaser.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraChaser : MonoBehaviour
+{
+    GameObject player;
+
+    private Vector3? chasingPos = null;
+    Vector3 startPos;
+    bool zoomingIn = false;
+    float progress;
+
+    public void SetGoal(Vector3 start, Vector3? goal, bool zoomingIn)
+    {
+        progress = 0;
+        startPos = start;
+        transform.position = startPos;
+        this.zoomingIn = zoomingIn;
+        chasingPos = goal;
+    }
+
+    Vector3 GoalPos()
+    {
+        if (chasingPos == null)
+        {
+            if (zoomingIn)
+            {
+                return new Vector3(player.transform.position.x, player.transform.position.y, 1);
+            } else
+            {
+                return new Vector3(player.transform.position.x, player.transform.position.y, -17.5f);
+            }
+        } else
+        {
+            return chasingPos.Value;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(transform.position.x, transform.position.y, 1f);
+        player = GameObject.Find("Player");
+
+        SetGoal(new Vector3(0, 0, 0), null, false);
+    }
+
+    public static float MapRange(float value, float inMin, float inMax, float outMin, float outMax)
+    {
+        return outMin + (value - inMin) * (outMax - outMin) / (inMax - inMin);
+    }
+
+    private float progressVelocity = 0.0f; // Velocity for SmoothDamp
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.rotation = player.transform.rotation;
+
+        progress = Mathf.SmoothDamp(progress, 1f, ref progressVelocity, 0.5f); // Smoothly transition progress to 1
+
+        transform.position = Vector3.Lerp(startPos, GoalPos(), progress);
+
+        ScreenDissolveFeature.Instance.progress = MapRange(transform.position.z, 1, -17.5f, 0, 1);
+        //if (reached)
+        //{
+        //    transform.position = GoalPos();
+        //} else
+        //{
+        //    transform.position = Vector3.MoveTowards(transform.position, GoalPos(), 5f * Time.deltaTime);
+        //}
+        //if (zoomingIn)
+        //{
+        //    transform.position = new Vector3(transform.position.x, transform.position.y, Mathf.MoveTowards(transform.position.z, 1f, 5f * Time.deltaTime));
+        //} else
+        //{
+        //    transform.position = new Vector3(transform.position.x, transform.position.y, Mathf.MoveTowards(transform.position.z, -17.5f, 5f * Time.deltaTime));
+        //}
+        //ScreenDissolveFeature.Instance.progress = MapRange(transform.position.z, 1, -17.5f, 0, 1);
+    }
+}
